397449fc03825b1ff6bf488bf97f1f43c6125cf8
hangueira <hks713@naver.com>
2021-09-29 20:30:36 +0900

    add video_list and video_item

---
+++ src/components/video_item/video_item.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const VideoItem = (props) => {
+  return (
+    <>
+      <h1>{props.video.snippet.title}</h1>
+      <img src={props.video.snippet.thumbnails.default.url} alt='' />
+    </>
+  );
+};
+
+export default VideoItem;
