7070f3e0b405e4a1c19b9c46a1c95ae1568b35fc
hangueira <hks713@naver.com>
2021-10-01 16:35:24 +0900

    make video item

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import styles from './video_item.module.css';
 
-const VideoItem = (props) => {
+const VideoItem = ({ video: { snippet } }) => {
   return (
-    <>
-      <h1>{props.video.snippet.title}</h1>
-      <img src={props.video.snippet.thumbnails.default.url} alt='' />
-    </>
+    <li className={styles.container}>
+      <div className={styles.video}>
+        <img
+          className={styles.thumbnail}
+          src={snippet.thumbnails.medium.url}
+          alt='video thumnail'
+        />
+        <div className={styles.metadata}>
+          <p className={styles.title}>{snippet.title}</p>
+          <p className={styles.channel}>{snippet.channelTitle}</p>
+        </div>
+      </div>
+    </li>
   );
 };
 
